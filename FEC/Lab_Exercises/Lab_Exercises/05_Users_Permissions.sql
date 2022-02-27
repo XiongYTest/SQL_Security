9198d9a65a92340c2ad9eb9efadc5f18df72cc24
SQLMCT <33372273+SQLMCT@users.noreply.github.com>
2022-02-27 12:28:51 -0500

    Move Files

--- FEC/Lab_Exercises/Lab_Exercises/05_Users_Permissions.sql
+++
@@ -1,9 +0,0 @@
---Check that SQL Server Authentication is enabled
-
---Create a SQL Login for with your name and assign a password.
-
---Create a Database user account mapped to the SQL Login.
-
---Assign permissions to your user account to the HR.Employees table.
-
---Assign permissions to your user account the Sales schema.
